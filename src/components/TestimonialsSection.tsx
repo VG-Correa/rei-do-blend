552523f344a0acefb65c86ca0caa689b457e9812
VG-Correa <v.victorgabriel2014@gmail.com>
2025-08-13 11:54:23 -0300

    Corrigido import

--- src/components/TestimonialsSection.tsx
+++ src/components/TestimonialsSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Star } from 'lucide-react';
 import { testimonials } from '../core/testimonials.mock';
 
