a06fba5beef412ca470a6de39393e2143922d31c
VG-Correa <v.victorgabriel2014@gmail.com>
2025-08-13 22:38:01 -0300

    Corrigido import de dependencia antiga

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import './App.css';
 import AppRoutes from './routes';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
-    <HelmetProvider>
+    <>
       <AppRoutes />
       <SpeedInsights />
-    </HelmetProvider>
+    </>
   );
 }
 
